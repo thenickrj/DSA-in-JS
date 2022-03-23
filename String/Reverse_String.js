3b004af3883f79f85682480511df6256cbc9f361
Nikhil Singh <nikhilbangalor@gmail.com>
2022-03-23 19:43:52 +0530

    Mar 23

---
+++ String/Reverse_String.js
@@ -0,0 +1,68 @@
+let s = ["h", "e", "l", "l", "o"];
+let s2 = ["H", "a", "n", "n", "a", "h"];
+let s3 = [
+  "A", //0
+  " ", //1
+  "m", //2
+  "a", //3
+  "n", //4
+  ",", //5
+  " ", //6
+  "a", //7
+  " ", //8
+  "p", //9
+  "l", //10
+  "a", //11
+  "n", //12
+  ",", //13
+  " ", //14
+  "a", //15
+  " ", //16
+  "c", //17
+  "a", //18
+  "n", //19
+  "a", //20
+  "l", //21
+  ":", //22
+  " ", //23
+  "P", //24
+  "a", //25
+  "n", //26
+  "a", //27
+  "m", //28
+  "a", //29
+];
+
+//Output    ["a","m","a","n","a","P"," ",":","l","a","n","a","c"," "," ","a",",","n","a","l","p"," ","a"," ",",","n","a","m"," ","A"]
+//Expected  ["a","m","a","n","a","P"," ",":","l","a","n","a","c"," ","a"," ",",","n","a","l","p"," ","a"," ",",","n","a","m"," ","A"]
+console.log(revStr(s3));
+
+function revStr(string) {
+  let start = 0;
+  let end = string.length - 1;
+  let temp;
+  if (string.length === 1) {
+    return string;
+  }
+  console.log(string[13]);
+
+  if (string.length % 2 === 0) {
+    while (start !== end - 1) {
+      temp = string[start];
+      string[start] = string[end];
+      string[end] = temp;
+      start++;
+      end--;
+    }
+  } else {
+    while (start < end) {
+      temp = string[start];
+      string[start] = string[end];
+      string[end] = temp;
+      start++;
+      end--;
+    }
+  }
+  //   console.log(string);
+  //   return string;
+}
