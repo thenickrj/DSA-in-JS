383fd95c62512bd76b0ea5cb9258c2ebede0f194
Nikhil Singh <nikhilbangalor@gmail.com>
2022-04-26 22:30:38 +0530

    2nd

---
+++ Algorithms/Sorting/InsertionSort.js
@@ -0,0 +1,16 @@
+let arr = [5, 4, 3, 2, 1];
+
+console.log(InsertionSort(arr));
+
+function InsertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let temp = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > temp) {
+      arr[j + 1] = arr[j];
+      j = j - 1;
+    }
+    arr[j + 1] = temp;
+  }
+  return arr;
+}
